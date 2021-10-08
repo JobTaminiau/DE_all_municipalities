ed8995902920ef301a090f3bde2b3cb97c00d42b
JobTaminiau <50639868+JobTaminiau@users.noreply.github.com>
2021-10-07 21:20:49 -0400

    First commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Project that aims to identify rooftop solar potential for all incorporated municipalities in Delaware.',
+    author='Job Taminiau',
+    license='',
+)
